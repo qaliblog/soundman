c83066f359129d1ee9166ee67733a2382dfc6ff8
qalisd <qaliblog9876@gmail.com>
2025-11-13 00:19:55 +0000

    full proofed

--- app/src/main/java/com/soundman/app/ui/components/Dialogs.kt
+++ app/src/main/java/com/soundman/app/ui/components/Dialogs.kt
@@ -3,6 +3,7 @@ package com.soundman.app.ui.components
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.soundman.app.data.PersonLabel
